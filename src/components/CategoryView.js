d1133ed1e26f55dd1f1393fc508feac489b034f8
Alan Sánchez <alan247@gmail.com>
2018-06-19 23:30:40 -0500

    Add remove/edit buttons to post items

--- src/components/CategoryView.js
+++ src/components/CategoryView.js
@@ -1,37 +1,71 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
-// import Icon from './Icon'
-// import {ICONS} from './IconPaths'
 import {connect} from 'react-redux'
 import Moment from 'react-moment'
 import SortBox from './SortBox'
-// import store from "../store"
 
+import {deletePost} from '../actions/posts'
 import VoteHandler from './VoteHandler'
 import Do from '../actions/types'
 import {getPosts} from '../actions/posts'
 
 
-const PostListItem = ({post, commentCount}) => {
-  const payload = {
-    type: Do.POST_VOTE,
-    postId: post.id
-  }
+class PostListItem extends Component {
 
-  return(
+
+  state = {redirect: false}
+
+
+
+  render(){
+
+    const {post, commentCount} = this.props
+
+     const redirect = (postId) => {
+      this.props.deletePost(postId)
+      window.location.reload()
+    }
+
+    const payload = {
+      type: Do.POST_VOTE,
+      postId: post.id
+    }
+
+
+    return(
     <li className="post-item">
+
 
         <h2><Link to={`/${post.category}/${post.id}`}>{post.title}</Link></h2>
 
         <div className="metadata">
+          <Link to={`/post/${post.id}/edit`}>
+            <button>Edit</button>
+          </Link>
+
+          <button
+          className="delete"
+          onClick={() => {redirect(post.id)}}
+          >Remove</button>
           <span className="comment-count"> {commentCount} {(commentCount === 1) ? "comment" : "comments"}</span>
           <span className="date-author">
             Submitted by {post.author} <Moment className="text-warning label" element="span" fromNow>{post.timestamp}</Moment> to <strong>{post.category}</strong>
           </span>
         </div>
     <VoteHandler payload={payload} voteCount={post.voteScore} />
-  </li>)
+  </li>
+  )
+  }
 }
+
+
+
+// const PostListItem = ({post, commentCount}) => {
+
+
+//   return(
+//     )
+// }
 
 class CategoryView extends Component {
 
@@ -45,8 +79,7 @@ class CategoryView extends Component {
   }
 
   render() {
-
-      const {allComments, match} = this.props
+      const {allComments, match, removePost} = this.props
       const category = match.params.category
       const posts = !category ?
       this.props.posts :
@@ -62,6 +95,7 @@ class CategoryView extends Component {
                 key={post.id}
                 post={post}
                 commentCount={this.commentCount(post.id, allComments)}
+                deletePost={removePost}
                 />)
               }
           </ul>
@@ -70,16 +104,26 @@ class CategoryView extends Component {
 
 
       )
-    }
+  }
 }
 
 const mapStateToProps = ({posts, allComments}) => {
   return {posts, allComments}
 }
 
-const loadPosts = () => (dispatch) => {
-  dispatch(getPosts())
+
+const mapDispatchToProps =  (dispatch) => {
+  return {
+    loadPosts() {
+      dispatch(getPosts())
+    },
+    removePost(data) {
+      dispatch(deletePost(data))
+    }
+  }
 }
 
-export default connect(mapStateToProps, {loadPosts})(CategoryView);
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(CategoryView, PostListItem);
 
